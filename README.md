f7b6b92900406d9b24a290b638856e053896d80a
Pier Luigi Ventre <pierventre@hotmail.com>
2014-03-28 17:59:48 +0100

    Improve Readme, add Copyright and License

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ Dreamer-Testbed-Deployer
 ========================
 
 Testbed Deployer For Dreamer Project (GÉANT Open Call)
+
+Using this tool you can "literally" program your Testbed
+configuration file. Moreover you can connect in remote to the VM
+leveraging the internal CLI or execute remote command on single VM
+or on a group of VM. 
+
+License
+=======
+
+This sofware is licensed under the Apache License, Version 2.0.
+
+Information can be found here:
+ [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+Tips
+==============
+
+When you choose the build of Topology from json file, the topology
+must be saved in topo folder
+
+When you use networkx the topo's png is saved in topo.png
+
+In testbed_deployer.py there are usage example of the Testbed Deployer
+
+Testbed Deployer Dependencies
+=============================
+
+0) Luca Prete's script
+
+1) networkx + its dependencies
+
+2) pygraphviz
+
+3) matplotlib
+
+Usage
+=====
+
--- testbed_deployer_utils.py
+++ testbed_deployer_utils.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python
 
-"""
-Utility Class For TestbedDeployer
-
-author: Pier Luigi Ventre (pl.ventre@gmail.com)
-author: Giuseppe Siracusano (a_siracusano@tin.it)
-author: Stefano Salsano (stefano.salsano@uniroma2.it)
-
-"""
+##############################################################################################
+# Copyright (C) 2014 Pier Luigi Ventre - (Consortium GARR and University of Rome "Tor Vergata")
+# Copyright (C) 2014 Giuseppe Siracusano, Stefano Salsano - (CNIT and University of Rome "Tor Vergata")
+# www.garr.it - www.uniroma2.it/netgroup - www.cnit.it
+#
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Deployer Utils.
+#
+# @author Pier Luigi Ventre <pl.ventre@gmail.com>
+# @author Giuseppe Siracusano <a_siracusano@tin.it>
+# @author Stefano Salsano <stefano.salsano@uniroma2.it>
+#
+# XXX Depends On Luca Prete Script
 
 from netaddr import *
 from ipaddress import *
